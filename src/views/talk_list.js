8c9c83a5ae4ead93dc8555029fa5477080d9e917
yasuhiroki <yasuhiroki.duck@gmail.com>
2019-11-03 23:59:59 +0900

    Modify header title

--- src/views/talk_list.js
+++ src/views/talk_list.js
@@ -5,7 +5,7 @@ module.exports = {
   oninit: Talk.loadList,
   view: function() {
     return m("article.center.mw5.mw6-ns.br3.hidden.ba.b--black-10.mv4", [
-      m("h1.f4.bg-near-white.br3.br--top.black-60.mv0.pv2.ph3", "Talk"),
+      m("h1.f4.bg-near-white.br3.br--top.black-60.mv0.pv2.ph3", "Talks"),
       m("div.pa3.bt.b--black-10",
         m("ul.list.pl0.mt0.center", Talk.list.map(function(talk) {
           return m("li.flex.items-center.lh-copy.pa3.ph0-l.bb.b--black-10", [
