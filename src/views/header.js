0d88fb9dcc722643be44ef5648f2d8ddaa1c62bb
yasuhiroki <yasuhiroki.duck@gmail.com>
2020-02-02 17:37:54 +0900

    Add twitter link

--- src/views/header.js
+++ src/views/header.js
@@ -6,7 +6,12 @@ module.exports = {
       m("img.br-100.h4.w4.dib.ba.b--black-05.pa2", {
         src: "https://avatars2.githubusercontent.com/u/3108110?v=4&s=460"
       }),
-      m("h1.f5.f4-ns.fw6.black-70", "yasuhiroki")
+      m("h1.f5.f4-ns.fw6.black-70", "yasuhiroki"),
+      m("h2.f6.black-70.fw2.ttu.tracked", [
+        "お仕事のご依頼は ",
+        m("a", {href: "https://twitter.com/duck_yasuhiroki"}, "Twitter"),
+        " DM にて"
+      ]),
     ]);
   }
 }
